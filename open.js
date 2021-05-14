4c79b2b9143cb74ac9295a10b7ee60baa23f5ced
Kevin Mei <kevjinx@gmail.com>
2021-05-13 17:41:04 -0700

    add new categories

--- open.js
+++ open.js
@@ -1,6 +1,9 @@
 const url = 'https://kevjinx.github.io/trivia-game/'
 window.addEventListener('DOMContentLoaded', () => {
-  window.open(url)
+  setTimeout(() => {
+    window.open(url)
+  }, 1000)
+
 })
 
 // just copied and pasted onto html, too lazy to
